815fe1ca0492fad2ca7966db81d4a8db1910ca41
hubiqing <hubiqing@jd.com>
2021-03-10 20:44:39 +0800

    ok

--- src/commands/subtree.ts
+++ src/commands/subtree.ts
@@ -76,7 +76,7 @@ ${stringify([new SubtreeModel({ prefix: 'common', url: 'http://xx.xx', alias: 'l
                 await exec(cmdStr, { cwd })
             } catch (e) {
                 consoleColor.error(e)
-                console.log(1111)
+                console.log(1111,e.message)
                 if (e.message.indexOf('already exists') == -1) {
                     consoleColor.green(`目录:${io.pathTool.resolve(subtree.prefix)}已存在,请处理`)
                 }
